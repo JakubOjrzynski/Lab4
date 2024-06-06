da7750f4b18021adda3df2cfa7bd312be7519de1
JakubOjrzynski <dsw51719@student.dsw.edu.pl>
2024-06-07 00:07:35 +0200

    dodanie --logs z drugim parametrem

--- skrypt.sh
+++ skrypt.sh
@@ -4,9 +4,18 @@ if [[ "$1" == "--date" ]]; then
     date
 fi
 
-if [[ "$1" == "--logs" ]]; then
+if [[ "$1" == "--logs" && -z "$2" ]]; then
     for i in {1..100}; do
         echo "Numer pliku $i" > "logx$i.txt"
         echo "stworzone przez $(whoami) w dniu $(date)" >> "logx$i.txt"
     done
 fi
+
+if [[ "$1" == "--logs" && -n "$2" ]]; then
+    num_files=$2
+    for i in $(seq 1 $num_files); do
+        echo "numer pliku $i" > "logx$i.txt"
+        echo "stworzony przez $(whoami) w dniu $(date)" >> "logx$i.txt"
+    done
+fi
+
