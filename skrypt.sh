e14e044865b01283f08bcbe7025e904de737ca9e
JakubOjrzynski <dsw51719@student.dsw.edu.pl>
2024-06-06 17:46:32 +0200

    dodanie --logs

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,10 @@
 if [[ "$1" == "--date" ]]; then
     date
 fi
+
+if [[ "$1" == "--logs" ]]; then
+    for i in {1..100}; do
+        echo "Numer pliku $i" > "logx$i.txt"
+        echo "stworzone przez $(whoami) w dniu $(date)" >> "logx$i.txt"
+    done
+fi
